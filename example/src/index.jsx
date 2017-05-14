f596600dc3735ed25181f90c473620650a26d20d
axetroy <troy450409405@gmail.com>
2017-05-15 00:54:56 +0800

    update

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,31 +1,67 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Async, { Resolve, Reject } from '../../lib/react-async';
+import Async, {
+  Resolve,
+  Reject,
+  Finally,
+  Pending
+} from '../../lib/react-async';
 
 const element = document.createElement('div');
 document.body.appendChild(element);
-render(
-  <div>
-    hello world
-    <Async
-      promise={
-        new Promise(function(resolve, reject) {
-          console.log('run promise');
-          setTimeout(() => {
-            reject();
-          }, 3000);
-        })
-      }
-    >
-      <div>hello aaa</div>
-      123
-      <Resolve>
-        promise resolve
-      </Resolve>
-      <Reject>
-        promise reject
-      </Reject>
-    </Async>
-  </div>,
-  element
-);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      promise: this.createPromise()
+    };
+  }
+  createPromise() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        if (Math.random() > 0.5) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, 1000);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ promise: this.createPromise() });
+          }}
+        >
+          Click and refresh
+        </button>
+        <Async await={this.state.promise}>
+          <div>async header, i will always here</div>
+          {component => {
+            return (
+              <div>
+                async status: {component.state.status}
+              </div>
+            );
+          }}
+          <Pending>promise pending</Pending>
+          <Resolve>
+            promise resolve block
+          </Resolve>
+          <Reject>
+            promise reject block
+          </Reject>
+          <Finally>
+            promise finally block
+          </Finally>
+        </Async>
+      </div>
+    );
+  }
+}
+
+render(<App />, element);
